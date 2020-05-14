e6591019ed92ca6ccf9d55230f3480a24bfee284
kyle L <laik@gilmour.org>
2020-05-14 22:15:22 +0800

    Character cards and log card

--- settings.py
+++ settings.py
@@ -26,7 +26,7 @@ SECURITY_CONFIRMABLE = os.environ.get("SECURITY_CONFIRMABLE", True)
 SECURITY_RECOVERABLE = os.environ.get("SECURITY_RECOVERABLE", True)
 SECURITY_PASSWORD_HASH = private.SECURITY_PASSWORD_HASH
 SECURITY_PASSWORD_SALT = private.SECURITY_PASSWORD_SALT
-SECURITY_POST_LOGIN_VIEW = '/'   # controls what page you see after login
+SECURITY_POST_LOGIN_VIEW = '/dashboard'   # controls what page you see after login
 SECURITY_EMAIL_SENDER = private.ADMIN_EMAIL # fixes error https://github.com/mattupstate/flask-security/issues/685
 
 STARTING_ADMINS = private.STARTING_ADMINS
